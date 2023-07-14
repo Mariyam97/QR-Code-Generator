4e0a25e298f2107826720dfbe9fc9d7ca6741b14
Mariyam <bibimariyamsardar1997@gmail.com>
2023-07-14 18:41:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import QRCode from "react-qr-code";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [qrVisible, setQrVisible] = useState(false);
+
+  const generateQrCodeHandler = () => {
+    if (!value) {
+      return;
+    }
+    setQrVisible(true);
+  };
+
+  const clearInputHandler = () => {
+    setValue("");
+    setQrVisible(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>QR Code Generator ☳</h1>
+      <input
+        type="text"
+        placeholder="Type a URL..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={generateQrCodeHandler}>Generate QR Code</button>
+      {qrVisible && (
+        <div className="qr-container">
+          <QRCode value={value} size={300} />
+        </div>
+      )}
+      {qrVisible && (
+  <button className="clear-button" onClick={clearInputHandler}>
+    Clear
+  </button>
+)}
+
     </div>
   );
 }
